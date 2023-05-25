4cd53be8a17ccb10cf340ce493a1d5369e7f7120
Adrian <adfonat88@yahoo.com>
2023-05-25 06:18:34 +0200

    add metamodel

--- webapp/controller/TestPage.controller.js
+++ webapp/controller/TestPage.controller.js
@@ -10,6 +10,11 @@ sap.ui.define([
             var oText = new Text;
             oText.setText("Text Control created in the controller");
             oPage.addContent(oText);
+
+            var oModel = new sap.ui.model.odata.v2.ODataModel("https://services.odata.org/TripPinRESTierService/(S(id))/");
+            var oMetaModel = oModel.getMetaModel();
+            var aEntitySets = oMetaModel.getEntitySetNames();
+            console.log(aEntitySets);
         },
         goBack: function () {
             this.onNavBack();
